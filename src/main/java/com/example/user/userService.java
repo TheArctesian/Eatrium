3f94bc54c3361d6bdde05d876f5cd9e0449800d8
theArctesian <stephen.d.okita@gmail.com>
2021-11-27 21:04:57 +0800

    init commit

---
+++ src/main/java/com/example/user/userService.java
@@ -0,0 +1,19 @@
+package com.example.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class userService {
+    public List<user> getUser() {
+        return List.of(
+                new user(
+                        1L,
+                        "edwin lagos",
+                        12,
+                        "elag@gmail.com"
+                )
+        );
+    }
+}
