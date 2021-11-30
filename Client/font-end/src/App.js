933d0d37cd7ff1319e0f9b51982b7d116edf6236
Stephen Okita <87273292+TheArctesian@users.noreply.github.com>
2021-12-01 05:49:49 +0800

    added

---
+++ Client/font-end/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ListMenuCompontent from './components/ListMenuCompontent'
+function App() {
+  return (
+    <div classname="container">
+      <ListMenuCompontent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/font-end/src/services/MenuService.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = "htttp://localhost:"; //write that 
+
+class MenuService{
+    return axios.get()
+}
